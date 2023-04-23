c25183196d76355bb0dc6c8db96df7da4c6eb230
Henry Fossett <henry.fossett@gmail.com>
2023-04-23 02:08:55 -0400

    updated files

---
+++ 2ndPopup.js
@@ -0,0 +1,7 @@
+let popup2 = document.getElementById("popup2");
+        function openPopup2(){
+            popup2.classList.add("open-popup2");
+        }
+        function closePopup2(){
+            popup2.classList.remove("open-popup2")
+        }
